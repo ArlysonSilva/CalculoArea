be805bd3a6a7ef1d0adc89b6a27d4cda570b936b
arlyson <arlyson2000@hotmail.com>
2025-04-15 00:25:29 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoArea/Calculo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoArea
+{
+    public class Calculo
+    {
+        private Double baseRetangulo;
+        
+        public Double BaseRetangulo
+        { get { return baseRetangulo; }
+          set { baseRetangulo = value; }
+        }
+        private Double alturaRetangulo;
+        
+        public Double AlturaRetangulo
+        { get { return alturaRetangulo; }
+          set { alturaRetangulo = value; }
+        }
+
+        //propriedade area
+        private double retangulo;
+        //metodo para calcular area
+        public void CalculoArea() 
+        {
+            retangulo = this.baseRetangulo * this.alturaRetangulo;
+        }
+
+        //metodo para exibir area calculada
+
+        public void ExibirArea()
+        {
+            Console.WriteLine("Area do retangulo: " + retangulo+"m²");
+        }
+
+        public void ExibirValores()
+        {
+            Console.WriteLine("A base vale = "+this.baseRetangulo+"m²\nA altura equivale = "+this.alturaRetangulo+"m²");
+
+        }
+    }
+}
---
+++ CalculoArea/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CalculoArea
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Calculo c = new Calculo();
+            int x;
+            Console.WriteLine("Vamos calcular a área de um retangulo:");
+            Console.WriteLine("Digite a altura em metros:");
+            c.AlturaRetangulo = Convert.ToDouble( Console.ReadLine());
+            Console.WriteLine("Digite a base em metros:");
+            c.BaseRetangulo = Convert.ToDouble( Console.ReadLine());
+            c.CalculoArea();
+            Console.WriteLine("Caso queira exibir o valor da àrea digite 1 caso queira ver os valores armazenados digite 2");
+            x = int.Parse( Console.ReadLine() );
+            switch (x) 
+            {
+                case 1:
+                    c.ExibirArea();
+                    break;
+
+                case 2:
+                    c.ExibirValores();
+                    break;
+                default:
+                    Console.WriteLine("Opção Invalida");
+                    break;
+            }
+
+
+        }
+    }
+}
