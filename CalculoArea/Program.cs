81083bb935ab2574ed9ff9c040fb7a0d6fccea1e
arlyson <arlyson2000@hotmail.com>
2025-04-15 00:27:32 -0300

    Alterações pequenas

--- CalculoArea/Program.cs
+++ CalculoArea/Program.cs
@@ -6,13 +6,13 @@
         {
             Calculo c = new Calculo();
             int x;
-            Console.WriteLine("Vamos calcular a área de um retangulo:");
+            Console.WriteLine("Vamos calcular a área de uma figura retangular:");
             Console.WriteLine("Digite a altura em metros:");
             c.AlturaRetangulo = Convert.ToDouble( Console.ReadLine());
             Console.WriteLine("Digite a base em metros:");
             c.BaseRetangulo = Convert.ToDouble( Console.ReadLine());
             c.CalculoArea();
-            Console.WriteLine("Caso queira exibir o valor da àrea digite 1 caso queira ver os valores armazenados digite 2");
+            Console.WriteLine("Caso queira exibir o valor da àrea digite 1;\nCaso queira ver os valores armazenados digite 2;");
             x = int.Parse( Console.ReadLine() );
             switch (x) 
             {
